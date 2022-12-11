986828c876c4179aeb42a6eebf5ea23cd5e85595
Natalya <nanona@example.com>
2022-12-11 14:23:47 +0700

    Added Zadacha2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдет сумму натуральных элементов в промежутке от M до N.
+
+int m = ReadInt("Введите M: ");
+int n = ReadInt("Введите N: ");
+int temp = m;
+
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+Console.Write($"M = {m}, N = {n} -> Сумма = ");
+PrintSum(m, n, temp = 0);
+
+void PrintSum(int m, int n, int sum)
+{
+    sum = sum + n;
+    if(n <= m)
+    {
+        Console.Write($"{sum}");
+        return;
+    }
+    PrintSum(m, n-1, sum);
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
