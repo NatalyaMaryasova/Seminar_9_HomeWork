349eb5f58c0c0934713bdfc30ac12ff97fd5f1d6
Natalya <nanona@example.com>
2022-12-11 14:23:29 +0700

    Added Zadacha1

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значение N. Напишмите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1.
+
+int n = ReadInt("Введите N: ");
+Console.WriteLine(NaturalNumber(n));
+
+int NaturalNumber(int n)
+{
+    if (n > 1)
+    {
+    Console.Write(n + ",");
+    NaturalNumber(n-1);
+    }
+    return 1;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
